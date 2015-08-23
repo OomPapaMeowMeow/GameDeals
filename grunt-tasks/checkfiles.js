9cb335c08c67997330d556ad18c28fd5b67ab3b1
Daniel Kamkha <daniel.kamkha@gmail.com>
2015-08-23 14:54:12 +0300

    Put checkfiles Grunt task into a separate file

---
+++ grunt-tasks/checkfiles.js
@@ -0,0 +1,65 @@
+//     This file is part of Game Deals extension for Google Chrome
+//     https://github.com/DanielKamkha/GameDealsChrome
+//     (c) 2015 Daniel Kamkha
+//     Game Deals is free software distributed under the terms of the MIT license.
+
+module.exports = function (grunt) {
+  "use strict";
+
+  grunt.registerMultiTask("checkfiles", "Check files mentioned in a .json for existence.", function() {
+    var pattern = this.data.pattern;
+    if (!pattern || !(pattern instanceof RegExp)) {
+      grunt.log.writeln("No pattern");
+      return;
+    }
+    var src = this.data.src;
+    if (!src || !grunt.file.exists(src)) {
+      grunt.log.writeln("Source not found");
+      return;
+    }
+    grunt.log.writeln("Src: " + src);
+    var srcDir = src.split("/").slice(0, -1).join("/");
+    grunt.file.setBase();
+    var dst = this.data.dst || src;
+    grunt.log.writeln("Dst: " + dst);
+
+    function checkFilesInArray(array) {
+      return array.reduce(function(result, val) {
+        if (typeof val === "string") {
+          if (pattern.test(val) && !grunt.file.exists(srcDir, val)) {
+            grunt.log.writeln("Deleting reference to " + val);
+            return result;
+          }
+        } else if (Array.isArray(val)) {
+          val = checkFilesInArray(val);
+        } else if (typeof val === "object") {
+          val = checkFilesInData(val);
+        }
+        result.push(val);
+        return result;
+      }, []);
+    }
+
+    function checkFilesInData(data) {
+      for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+          var val = data[key];
+          if (typeof val === "string") {
+            if (pattern.test(val) && !grunt.file.exists(srcDir, val)) {
+              grunt.log.writeln("Deleting reference to " + val);
+              delete data[key];
+            }
+          } else if (Array.isArray(val)) {
+            data[key] = checkFilesInArray(val);
+          } else if (typeof val === "object") {
+            data[key] = checkFilesInData(val);
+          }
+        }
+      }
+      return data;
+    }
+
+    var srcData = checkFilesInData(grunt.file.readJSON(src));
+    grunt.file.write(dst, JSON.stringify(srcData, null, 2));
+  });
+};
